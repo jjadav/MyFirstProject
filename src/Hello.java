8de82fa84fb9027bba38367d00b1291585d0ad25
jjadav <jjadav@mamkschools.org>
2018-09-05 23:55:36 -0400

    Added .gitignore file to the directory

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-   }
+        System.out.println("After sucessful commit!");
+        System.out.println("Push request successful!");
+    }
 }
